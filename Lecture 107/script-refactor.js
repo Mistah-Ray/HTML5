28a38be138988a3bca67302838253e5a5d6b5b1f
devRay <BornInFlamez@aol.com>
2018-11-06 03:04:47 -0500

    Lecture 113 Update
    11/6/2018

--- Lecture 107/script-refactor.js
+++ Lecture 107/script-refactor.js
@@ -2,29 +2,31 @@ var button = document.getElementById("enter");
 var item = document.getElementById("userItem");
 var ul = document.getElementById("toDoList");
 
-button.addEventListener("click", addToList);
 
-function addToList()
+function addListItem()
+{
+		var li = document.createElement("li");
+		li.appendChild(document.createTextNode(item.value));
+		ul.appendChild(li);
+		document.getElementById("userItem").value = null;
+}
+
+function addByClick()
 {
 	if(item.value.length > 0)
 	{
-		var li = document.createElement("li");
-		li.appendChild(document.createTextNode(item.value));
-		ul.appendChild(li);
-		document.getElementById("userItem").value = null;
+		addListItem();
 	}
 }
 
-item.addEventListener("keypress", addToListK);
 
-function addToListK(e)
+function addByEnter(e)
 {
 	if(item.value.length > 0 && e.keyCode === 13)
 	{
-		var li = document.createElement("li");
-		li.appendChild(document.createTextNode(item.value));
-		ul.appendChild(li);
-		document.getElementById("userItem").value = null;
+		addListItem();
 	}
 }
 
+button.addEventListener("click", addByClick);
+item.addEventListener("keypress", addByEnter);
