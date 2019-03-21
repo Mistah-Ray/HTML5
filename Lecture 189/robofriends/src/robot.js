0060c664e3fdc70502004a7d6de0d7cd778a58b9
Mistah-Ray <Mistah-Ray@users.noreply.github.com>
2019-03-21 16:33:53 -0400

    Updated after React Section

---
+++ Lecture 189/robofriends/src/robot.js
@@ -0,0 +1,74 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Mistah Ray',
+        username: 'Mistah',
+        email: 'MistahRay@aol.com'
+    },
+    {
+        id: 2,
+        name: 'Mistah Dev',
+        username: 'Dev',
+        email: 'MistahDev@aol.com'
+    },
+    {
+        id: 3,
+        name: 'Jane Doe',
+        username: 'JaneD',
+        email: 'MJD@aol.com'
+    },
+    {
+        id: 4,
+        name: 'Dev Ram',
+        username: 'DRam',
+        email: 'DevR@aol.com'
+    },
+    {
+        id: 5,
+        name: 'Kelly Kills',
+        username: 'kKelly',
+        email: 'Kkells@aol.com'
+    },
+    {
+        id: 6,
+        name: 'Damien Mars',
+        username: 'DM',
+        email: 'DM@aol.com'
+    },
+    {
+        id: 1,
+        name: 'Mistah Ray',
+        username: 'Mistah',
+        email: 'MistahRay@aol.com'
+    },
+    {
+        id: 2,
+        name: 'Mistah Dev',
+        username: 'Dev',
+        email: 'MistahDev@aol.com'
+    },
+    {
+        id: 3,
+        name: 'Jane Doe',
+        username: 'JaneD',
+        email: 'MJD@aol.com'
+    },
+    {
+        id: 4,
+        name: 'Dev Ram',
+        username: 'DRam',
+        email: 'DevR@aol.com'
+    },
+    {
+        id: 5,
+        name: 'Kelly Kills',
+        username: 'kKelly',
+        email: 'Kkells@aol.com'
+    },
+    {
+        id: 6,
+        name: 'Damien Mars',
+        username: 'DM',
+        email: 'DM@aol.com'
+    }
+]
